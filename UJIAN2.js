95ec7b763be05afa07c29fbcfedf282ee3a62b53
faizanshar <muhammadfaizanshar@gmai.com>
2020-09-12 07:08:13 +0700

    first commit

---
+++ UJIAN2.js
@@ -0,0 +1,23 @@
+var listDesa = [
+    'argodadi,sedayu',
+    'argomulyo,sedayu',
+    'argorejo,sedayu',
+    'argosari',
+    'bangunjiwo,kasihan',
+    'banguntapan,banguntapan',
+    'bantul,bantul',
+    'baturetno,banguntapan',
+    'bawuran,pleret',
+    'canden,jetis',
+    'caturharjo,pandak',
+    'cetan srigandi'
+]
+
+var total = 0;
+
+
+
+for(i = 0; i < listDesa.length; i++){
+    
+    document.write(i+1 + '. ' + listDesa[i]+ '<br>')    
+}
